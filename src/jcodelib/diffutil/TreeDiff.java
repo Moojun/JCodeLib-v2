45d0e539f13cb307523257ef17b76fd6de39f78a
Moojun <mgim32137@gmail.com>
2023-02-25 00:10:55 +0900

    commit of JCodeLib: change some of the project structure

---
+++ src/jcodelib/diffutil/TreeDiff.java
@@ -0,0 +1,266 @@
+package jcodelib.diffutil;
+
+import ch.uzh.ifi.seal.changedistiller.ChangeDistiller;
+import ch.uzh.ifi.seal.changedistiller.ChangeDistiller.Language;
+import ch.uzh.ifi.seal.changedistiller.distilling.FileDistiller;
+import ch.uzh.ifi.seal.changedistiller.model.classifiers.SourceRange;
+import ch.uzh.ifi.seal.changedistiller.model.entities.*;
+import com.github.gumtreediff.actions.ActionGenerator;
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.gen.Generators;
+import com.github.gumtreediff.matchers.Matcher;
+import com.github.gumtreediff.matchers.Matchers;
+import com.github.gumtreediff.tree.ITree;
+import edu.fdu.se.cldiff.CLDiffLocal;
+import file.FileIOManager;
+import jcodelib.element.CDChange;
+import jcodelib.element.GTAction;
+import jcodelib.util.CodeUtils;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import script.ScriptGenerator;
+import script.model.EditScript;
+import tree.Tree;
+import tree.TreeBuilder;
+import tree.TreeNode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class TreeDiff {
+
+	public static List<SourceCodeChange> diffChangeDistiller(File left, File right){
+
+		FileDistiller distiller = ChangeDistiller.createFileDistiller(Language.JAVA);
+		try {
+			distiller.extractClassifiedSourceCodeChanges(left, right);
+		} catch(Exception e) {
+			/* An exception most likely indicates a bug in ChangeDistiller. Please file a
+		       bug report at https://bitbucket.org/sealuzh/tools-changedistiller/issues and
+		       attach the full stack trace along with the two files that you tried to distill. */
+			System.err.println("Warning: error while change distilling. " + e.getMessage());
+		}
+
+		List<SourceCodeChange> changes = distiller.getSourceCodeChanges();
+		List<SourceCodeChange> filteredChanges = new ArrayList<>();
+		if(changes != null) {
+			for(SourceCodeChange change : changes) {
+				if(!change.getChangedEntity().getType().isComment()){
+					filteredChanges.add(change);
+				}
+			}
+		}
+
+		return filteredChanges;
+	}
+
+	public static List<CDChange> diffCDChanges(File left, File right) throws IOException {
+		List<SourceCodeChange> changes = diffChangeDistiller(left, right);
+		return convertSourceCodeChanges(changes);
+	}
+
+	public static List<CDChange> convertSourceCodeChanges(List<SourceCodeChange> changes) throws IOException {
+		//Convert CDChange with new change type.
+		List<CDChange> converted = new ArrayList<>();
+		for(SourceCodeChange scc : changes) {
+			SourceRange range = scc.getChangedEntity().getSourceRange();
+			String changeType = scc.getChangeType().toString();
+			if(scc instanceof Insert) {
+				changeType = CDChange.INSERT;
+			}else if(scc instanceof Delete) {
+				changeType = CDChange.DELETE;
+			}else if(scc instanceof Move) {
+				changeType = CDChange.MOVE;
+			}else if(scc instanceof Update) {
+				changeType = CDChange.UPDATE;
+			}
+			converted.add(new CDChange(changeType, scc.getChangedEntity().getType().toString(),
+					range.getStart(), range.getEnd()));
+		}
+		return converted;
+	}
+
+	public static void updateEntityTypes(File leftFile, File rightFile, List<CDChange> changes) throws IOException {
+		//Create maps for start/end positions.
+		Tree left = TreeBuilder.buildTreeFromFile(leftFile);
+		Tree right = TreeBuilder.buildTreeFromFile(rightFile);
+		Map<Integer, TreeMap<Integer, String>> leftTypeMap = createNodeTypeMap(left.getRoot());
+		Map<Integer, TreeMap<Integer, String>> rightTypeMap = createNodeTypeMap(right.getRoot());
+
+		//Search for corresponding TreeNode using start/end positions.
+		for(CDChange c : changes) {
+			//Check left or right based on change type. If missing, use Unknown# + original.
+			String entityType = null;
+			switch(c.getChangeType()) {
+			case CDChange.INSERT:
+				entityType = findClosestEntity(rightTypeMap, c.getStartPos(), c.getEndPos());
+				break;
+			case CDChange.DELETE:
+			case CDChange.MOVE:
+			case CDChange.UPDATE:
+				entityType = findClosestEntity(leftTypeMap, c.getStartPos(), c.getEndPos());
+			}
+			c.setEntityType(entityType == null ? "Unknown#"+c.getEntityType() : entityType);
+		}
+	}
+
+	private static String findClosestEntity(Map<Integer, TreeMap<Integer, String>> map, int start, int end) {
+		//Find an entity which has the same start position and closest end position.
+		if(!map.containsKey(start))
+			return null;
+		Integer closest = map.get(start).ceilingKey(end);
+		return closest != null ? map.get(start).get(closest) : null;
+	}
+
+	private static Map<Integer, TreeMap<Integer, String>> createNodeTypeMap(TreeNode n) {
+		Map<Integer, TreeMap<Integer, String>> map = new HashMap<>();
+		createNodeTypeMap(map, n);
+		return map;
+	}
+
+	private static void createNodeTypeMap(Map<Integer, TreeMap<Integer, String>> map, TreeNode n) {
+		if(n != null && n.getASTNode() != null) {
+			int start = n.getStartPosition();
+			int end = n.getEndPosition();
+			if(!map.containsKey(start))
+				map.put(start, new TreeMap<>());
+			map.get(start).put(end, CodeUtils.getTypeName(n.getType()));
+		}
+		for(TreeNode child : n.children) {
+			createNodeTypeMap(map, child);
+		}
+	}
+
+	public static List<Action> diffGumTree(File srcFile, File dstFile) throws Exception {
+		List<Action> actions = null;
+		com.github.gumtreediff.client.Run.initGenerators();
+		ITree src = Generators.getInstance().getTree(srcFile.getAbsolutePath()).getRoot();
+		ITree dst = Generators.getInstance().getTree(dstFile.getAbsolutePath()).getRoot();
+		Matcher m = Matchers.getInstance().getMatcher(src, dst); // retrieve the default matcher
+		m.match();
+		ActionGenerator g = new ActionGenerator(src, dst, m.getMappings());
+		g.generate();
+		actions = g.getActions();
+
+		return actions;
+	}
+
+	public static List<GTAction> groupGumTreeActions(File srcFile, File dstFile, List<Action> actions, long timeInput) {
+		List<GTAction> gtActions = new ArrayList<>();
+		try {
+			CompilationUnit srcCu = CodeUtils.getCompilationUnit(FileIOManager.getContent(srcFile));
+			CompilationUnit dstCu = CodeUtils.getCompilationUnit(FileIOManager.getContent(dstFile));
+			//Group actions.
+			while(actions.size() > 0){
+				Action action = actions.get(0);
+				GTAction gtAction = new GTAction(action, srcCu, dstCu);
+				gtAction = attachActions(gtAction, actions, srcCu, dstCu);
+				gtAction.runTimeOfEditScript = timeInput;
+				gtActions.add(gtAction);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return gtActions;
+	}
+
+	public static List<GTAction> diffGumTreeWithGrouping(File srcFile, File dstFile) throws Exception {
+		long startTime = System.currentTimeMillis();
+		List<GTAction> gtActions = new ArrayList<>();
+		com.github.gumtreediff.client.Run.initGenerators();
+		ITree src = Generators.getInstance().getTree(srcFile.getAbsolutePath()).getRoot();
+		ITree dst = Generators.getInstance().getTree(dstFile.getAbsolutePath()).getRoot();
+		Matcher m = Matchers.getInstance().getMatcher(src, dst); // retrieve the default matcher
+		m.match();
+		ActionGenerator g = new ActionGenerator(src, dst, m.getMappings());
+		g.generate();
+		long finishTime = System.currentTimeMillis();
+		long elapsedTime = finishTime - startTime;
+
+		List<Action> actions = g.getActions();
+
+		//Group actions.
+		gtActions = groupGumTreeActions(srcFile, dstFile, actions, elapsedTime);
+
+		return gtActions;
+	}
+
+	private static GTAction attachActions(
+			GTAction gtAction, List<Action> actions, CompilationUnit srcCu, CompilationUnit dstCu) {
+		GTAction root = gtAction;
+
+		//Bottom-up search to find a root action.
+		GTAction parent;
+		ITree parentNode;
+		do {
+			parent = null;
+			parentNode = root.action.getNode().getParent();
+			if (parentNode != null) {
+				for (Action action : actions) {
+					if (action.getNode().getId() == parentNode.getId()
+							&& GTAction.getActionType(action).equals(root.actionType)) {
+						parent = new GTAction(action, srcCu, dstCu);
+						break;
+					}
+				}
+			}
+			//Switch the root.
+			if(parent != null){
+				root = parent;
+			}
+		} while (parent != null);
+
+		//Top-down search for children.
+		List<GTAction> targetActions = new ArrayList<>();
+		List<GTAction> attachedActions = new ArrayList<>();
+		targetActions.add(root);
+		actions.remove(root.action);
+		do {
+			targetActions.addAll(attachedActions);
+			attachedActions.clear();
+			//Find children of each target.
+			for(GTAction target : targetActions){
+				for (ITree child : target.action.getNode().getChildren()) {
+					for (Action action : actions) {
+						if (action.getNode().getId() == child.getId()
+								&& target.actionType.equals(GTAction.getActionType(action))) {
+							GTAction gta = new GTAction(action, srcCu, dstCu);
+							target.children.add(gta);
+							attachedActions.add(gta);
+							break;
+						}
+					}
+				}
+			}
+			//Remove all attached actions.
+			for(GTAction gta : attachedActions){
+				actions.remove(gta.action);
+			}
+		} while (attachedActions.size() > 0 && actions.size() > 0);
+
+		return root;
+	}
+
+	public static EditScript diffLAS(File srcFile, File dstFile){
+		try {
+			Tree before = TreeBuilder.buildTreeFromFile(srcFile);
+			Tree after = TreeBuilder.buildTreeFromFile(dstFile);
+			EditScript script = ScriptGenerator.generateScript(before, after);
+			return script;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static void runCLDiff(File srcFile, File dstFile) {
+
+	}
+
+	public static void runCLDiff(String repo, String commitId, String outputDir) {
+		CLDiffLocal CLDiffLocal = new CLDiffLocal();
+		CLDiffLocal.run(commitId,repo,outputDir);
+	}
+}
---
+++ src/main/java/CollectChanges.java
@@ -0,0 +1,238 @@
+package main.java;
+
+import db.DBManager;
+import file.FileIOManager;
+import jcodelib.diffutil.TreeDiff;
+import jcodelib.element.GTAction;
+import jcodelib.jgit.ReposHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectChanges {
+	//Hard-coded projects - need to read it from DB.
+	//public static String[] projects;
+	public static String baseDir = "subjects";
+	public static final String oldDir = "old";
+	public static final String newDir = "new";
+	public static long commitCount = 1;
+
+	public final static Logger log = LoggerFactory.getLogger(CollectChanges.class);
+	
+	public static void main(String[] args) {
+
+		//Select SCD Tool here.
+		//String tool = "ChangeDistiller";
+		String tool = "GumTree";
+		DBManager db = null;
+		
+		try {
+			//Change db.properties.
+			db = new DBManager("db.properties");
+
+			//Connect DB.
+			Connection con = db.getConnection();
+
+			// if exception occurs, do rollback
+			con.setAutoCommit(false);
+
+			/**
+			 * Replace the below code with Command Line args
+			 */
+			/*
+			//Collect project name.
+			PreparedStatement pstmt = con.prepareStatement("SELECT name FROM subjects");
+			ResultSet resultSet = pstmt.executeQuery();
+
+            StringBuilder tmp = new StringBuilder();
+
+            resultSet.next();
+            tmp.append(resultSet.getString("name"));
+
+            while (resultSet.next()) {
+                tmp.append(",").append(resultSet.getString("name"));
+            }
+
+            projects = tmp.toString().split(",");
+
+            resultSet.close();
+			pstmt.close();
+			 */
+
+			// Collect and store file_id, tool, change_type, entity_type, start_pos, script, run_time in DB.
+			PreparedStatement psIns = con.prepareStatement("insert into changes_GT " +
+					" ( file_id, tool, change_type, entity_type, start_pos, script ) " +
+					" values ( ?, ?, ?, ?, ?, ? )");
+
+			// Collect run time
+			PreparedStatement psTime = con.prepareStatement("insert into changes_GT_runtime " +
+					" (file_id, runtime_gitReset, runtime_editScript)" +
+					" values(?, ?, ?)");
+
+			// Collect Practically these files are deleted/inserted.
+			PreparedStatement psDeletedFileId = con.prepareStatement("insert into changes_GT_deleted " +
+					" (file_id, project_name, commit_id, old_commit, new_commit) +" +
+					" values(?, ?, ?, ?, ?) " );
+
+			// use command line arguments
+			String project = args[0];
+
+			System.out.println("Collecting Changes from " + project);
+			String oldReposPath = String.join(File.separator, baseDir, oldDir, project) + File.separator;
+			String newReposPath = String.join(File.separator, baseDir, newDir, project) + File.separator;
+			File oldReposDir = new File(oldReposPath);
+			File newReposDir = new File(newReposPath);
+
+			// Prepare files.
+			List<String> fileInfo = new ArrayList<>();
+
+			PreparedStatement fileSel = con.prepareStatement("select c.commit_id commit_id, c.old_commit old_commit, c.new_commit new_commit, " +
+					" f.file_path file_path, f.file_id file_id " +
+					" from commits c, files f where c.commit_id = f.commit_id and c.project_name = '" + project + "'" +
+					" order by file_id, commit_id ");
+			ResultSet fileRS = fileSel.executeQuery();
+			while (fileRS.next()) {
+				String str = String.join(",",
+				String.valueOf(fileRS.getInt("commit_id")),
+				fileRS.getString("old_commit"),
+				fileRS.getString("new_commit"),
+				fileRS.getString("file_path"),
+				String.valueOf(fileRS.getInt("file_id")));
+				fileInfo.add(str);
+			}
+
+			fileRS.close();
+			fileSel.close();
+
+			System.out.println("Total " + fileInfo.size() + " revisions.");
+
+			for (int i = 0; i < fileInfo.size(); i++) {
+
+				String key = fileInfo.get(i);
+				String[] tokens = key.split(",");
+				String commitId = tokens[0];
+				String oldCommitId = tokens[1];
+				String newCommitId = tokens[2];
+				String filePath = tokens[3];
+				String fileId = tokens[4];
+				System.out.println("CommitId : " + commitId + ", fileId : " + fileId + ", oldCommitId : " + oldCommitId + ", newCommitId : " + newCommitId);
+
+				// Reset hard to old/new commit IDs.
+				long gitResetStartTime = System.currentTimeMillis();
+				ReposHandler.update(oldReposDir, oldCommitId);
+				ReposHandler.update(newReposDir, newCommitId);
+				long gitResetFinishTime = System.currentTimeMillis();
+				long gitResetElapsedTime = gitResetFinishTime - gitResetStartTime;
+
+				try {
+					if(!filePath.contains("/org/"))
+						continue;
+					File oldFile = new File(oldReposPath + filePath);
+					File newFile = new File(newReposPath + filePath);
+					String oldCode = FileIOManager.getContent(oldFile).intern();
+					String newCode = FileIOManager.getContent(newFile).intern();
+					if (oldCode.length() == 0 || newCode.length() == 0) {
+						//Practically these files are deleted/inserted.
+						//System.out.println("File " + fileId + "s are practically deleted or inserted.");
+						psDeletedFileId.clearParameters();
+						psDeletedFileId.setInt(1, Integer.parseInt(fileId));
+						psDeletedFileId.setString(2, project);
+						psDeletedFileId.setString(3, commitId);
+						psDeletedFileId.setString(4, oldCommitId);
+						psDeletedFileId.setString(5, newCommitId);
+						psDeletedFileId.addBatch();
+						continue;
+					}
+
+					// Apply Source Code Differencing Tools.
+					// GumTree
+					List<GTAction> gumtreeChanges = TreeDiff.diffGumTreeWithGrouping(oldFile, newFile);
+
+					for(GTAction c : gumtreeChanges) {
+						psIns.clearParameters();
+						psIns.setString(1, fileId); //file_id
+						psIns.setString(2, tool); //tool: GT, etc..
+						psIns.setString(3, c.actionType); // change_type
+						psIns.setString(4, c.codeType); // entity_type
+						psIns.setInt(5, c.startPos); // start_pos
+						psIns.setString(6, c.toString()); // script
+						psIns.addBatch();
+					}
+
+					for (GTAction c : gumtreeChanges) {
+						psTime.clearParameters();
+						psTime.setInt(1, Integer.parseInt(fileId)); //file_id
+						psTime.setLong(2, gitResetElapsedTime);
+						psTime.setLong(3, c.runTimeOfEditScript);
+						psTime.addBatch();
+					}
+
+					if (commitCount % 100 == 0) {
+						psDeletedFileId.executeBatch();
+						psDeletedFileId.clearBatch();
+						psIns.executeBatch();
+						psIns.clearBatch();
+						psTime.executeBatch();
+						psTime.clearBatch();
+						con.commit();
+					}
+
+					// Committing for the rest of the syntax that has not been committed
+					psDeletedFileId.executeBatch();
+					psIns.executeBatch();
+					psTime.executeBatch();
+					con.commit();
+					psDeletedFileId.clearBatch();
+					psIns.clearBatch();
+					psTime.clearBatch();
+
+					// ChangeDistiller
+//						List<SourceCodeChange> changes = TreeDiff.diffChangeDistiller(oldFile, newFile);
+//						int runTime = TreeDiff.runTime;
+//						for(SourceCodeChange c : changes) {
+//
+//							psIns.clearParameters();
+//							psIns.setString(1, fileId); //fileId
+//							psIns.setString(2, tool); //tool
+//							psIns.setString(3, c.getChangeType().toString()); //change_type
+//							psIns.setString(4, c.getChangedEntity().toString()); //entity_type
+//							psIns.setInt(5, 0); //start_pos
+//							psIns.setString(6, c.toString()); //script
+//							psIns.setInt(7, runTime); //run_time
+//							psIns.addBatch();
+//						}
+
+				} catch (Exception e) {
+					log.error("In project {}, running error while processing fileId : {}, " +
+									"commitId : {}, oldCommitId : {}, newCommitId : {}",
+							project, fileId, commitId, oldCommitId, newCommitId);
+					e.printStackTrace();
+					try {
+						// If failed, do rollback
+						con.rollback();
+						//log.error("In project {}, commit is failed, so rollback is worked!", project);
+					} catch (SQLException E) {
+						E.printStackTrace();
+					}
+				}
+
+			}
+
+			psDeletedFileId.close();
+			psTime.close();
+			psIns.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.close();
+		}
+	}
+}
